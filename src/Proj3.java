a7e0624b0a6db2b11efd8bade2c1d4478b1276a3
iLuvMelon <dylankane0117@gmail.com>
2025-11-12 10:16:44 -0500

    so far

--- src/Proj3.java
+++ src/Proj3.java
@@ -1,7 +1,11 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+import java.io.File;
 
 public class Proj3 {
     // Sorting Method declarations
@@ -51,38 +55,31 @@ public class Proj3 {
 
     // Quick Sort
     public static <T extends Comparable> void quickSort(ArrayList<T> a, int left, int right) {
-        //this algorithm will select the middle as the pivot
         if (left < right) {
-            int pivot_index = partition(a, left, right);
-
-            quickSort(a, left, right);
+            int low_index = partition(a, left, right);
+            quickSort(a, left, low_index);
+            quickSort(a, low_index + 1, right);
         }
-
     }
 
-    public static <T extends Comparable> int partition (ArrayList<T> a, int left, int right) {
-        if (left < right) {
 
-            int mid = (left + right) / 2;
+    public static <T extends Comparable> int partition(ArrayList<T> a, int left, int right) {
 
-            ArrayList<T> lowerVals = new ArrayList<>();
-            ArrayList<T> upperVals = new ArrayList<>();
+        int p = right;
+        int j = right - 1;
+        int i = left;
 
-            for (int i = 0; i <= left + right; i++) {
-                if (i == mid) continue;
-
-                if (a.get(i).compareTo(a.get(mid)) > 0) upperVals.add(a.get(i));
-                else lowerVals.add(a.get(i));
-            }
-
-            ArrayList<T> finalArray = new ArrayList<>(lowerVals);
-            finalArray.add(a.get(mid));
-            finalArray.addAll(upperVals);
-
-            return finalArray.indexOf(a.get(mid));
-
+        while (i < j && i < a.size() && j >= left) {
+            //move i to the right til value greater than pivot is found
+            while (a.get(i).compareTo(a.get(p)) < 0) i++;
+            //move j to the left til value lower than pivot is found
+            while (j > left && a.get(j).compareTo(a.get(p)) > 0) j--;
+            if (i < j) swap(a, i, j);
         }
-        return 0;
+
+        swap(a, i, p);
+        return i;
+
     }
 
     static <T> void swap(ArrayList<T> a, int i, int j) {
@@ -93,52 +90,41 @@ public class Proj3 {
 
     // Heap Sort
     public static <T extends Comparable> void heapSort(ArrayList<T> a, int left, int right) {
-        // Finish Me
+        // first need to heapify the array
+        heapify(a, (left + right) / 2, left, right);
     }
 
     //MODIFICATIONS: added a curr variable
     public static <T extends Comparable> void heapify (ArrayList<T> a, int curr, int left, int right) {
 
         //checks if heapifying is finished
-        if (curr <= 0) return;
+        if (left >= a.size()) return;
 
         //store c, as it will be needed for the recursive call
         int c = curr;
 
         //sift down
-        try {
-            while (a.get(curr).compareTo(a.get(left)) > 0 || a.get(curr).compareTo(a.get(right)) > 0) {
+        while (true) {
 
-                //swap the minimum element
-                //case 1: right child is smaller
-                if (a.get(left).compareTo(a.get(right)) > 0) {
-                    swap(a, curr, right);
-                    curr = right;
-                    left = (2 * curr);
-                    right = (2 * curr) + 1;
-                }
+            int toSwap;
 
-                //case 2: right child is smaller
-                else {
-                    swap(a, curr, left);
-                    curr = left;
-                    left = (2 * curr);
-                    right = (2 * curr) + 1;
-                }
-            }
-        } catch (IndexOutOfBoundsException e) { //exception will occur if either left or right child is out of bounds
-            if (left <= a.size() && a.get(curr).compareTo(a.get(left)) > 0) {
-                swap(a, curr, left);
-            }
+            if (a.get(2 * (curr) - 1) != null) {
+                toSwap = left;
+                if (a.get(right) != null && a.get(left).compareTo(a.get(right)) < 0) toSwap = right;
+                if (a.get(toSwap).compareTo(a.get(curr)) > 0) swap(a, curr, toSwap);
+                curr = toSwap;
+
+            } else break;
+
         }
-
-        heapify(a, c - 1, 2 * (c - 1), (2 * (c - 1)) + 1);
+        heapify(a, c - 2, (2 * (c - 1)) -1, (2 * (c - 1)));
 
     }
 
     // Bubble Sort
-    //MODIFICATIONS: switched the return statement to a void
-    public static <T extends Comparable> void bubbleSort(ArrayList<T> a, int size) {
+    public static <T extends Comparable> int bubbleSort(ArrayList<T> a, int size) {
+
+        int numSwaps = 0;
         //first loop
         for (int i = 0; i < size; i++) {
             //second nested loop
@@ -147,30 +133,55 @@ public class Proj3 {
                 if (a.get(j + 1).compareTo(a.get(j)) < 0) {
                     swap(a, j, j + 1);
                     swapped = true;
+                    numSwaps++;
                 }
             }
             //end if there were no swaps in a pass
             if (!swapped) break;
         }
+
+        //returns number of swaps
+        return numSwaps;
     }
 
     // Odd-Even Transposition Sort
     public static <T extends Comparable> int transpositionSort(ArrayList<T> a, int size) {
-        for (int i = 0; i < a.size(); i++) {
+
+        int numSwaps = 0;
+        for (int i = 0; i < size; i++) {
+            boolean swapped = false;
             if (i % 2 == 0) { //even
-                for (int j = 0; j < a.size(); j += 2) {
+                for (int j = 0; j < a.size() - 1; j += 2) {
                     if (a.get(j).compareTo(a.get(j + 1)) > 0) {
-                        swap(a, i, j);
+                        swap(a, j, j + 1);
+                        numSwaps++;
+                        swapped = true;
                     }
                 }
             } else { //odd
-                for (int k = 1; k < a.size(); k += 2) {
+                for (int k = 1; k < a.size() - 1; k += 2) {
                     if (a.get(k).compareTo(a.get(k + 1)) > 0) {
-                        swap(a, i, k);
+                        swap(a, k, k + 1);
+                        numSwaps++;
+                        swapped = true;
                     }
                 }
+                //stop when list is sorted
+                if (!swapped) break;
             }
+
         }
+
+        //returns number of swaps
+        return numSwaps;
+    }
+
+    public static <T extends Comparable>  ArrayList<T> chooseArray(ArrayList<T> a, int i) {
+        ArrayList<T> arrayData = new ArrayList<>(a);
+        if (i == 0) Collections.shuffle(arrayData);
+        if (i == 1) Collections.sort(arrayData);
+        if (i == 2) Collections.reverse(arrayData);
+        return arrayData;
     }
 
     public static void main(String [] args)  throws IOException {
@@ -212,8 +223,110 @@ public class Proj3 {
             arrayData.add(currData);
         }
 
+        ArrayList<DataObj> arrayToSort;
+
+        long startTimer;
+        long endTimer;
+
+        /*
+        MAKE LISTS OF EACH SORTING TIME
+            0. Shuffled
+            1. Sorted
+            2. Reversed
+        */
+
+        double[] quickSortTimes = new double[3];
+        double[] mergeSortTimes = new double[3];
+        double[] oddEvenTimes = new double[3];
+        double[] heapSortTimes = new double[3];
+        double[] bubbleSortTimes = new double[3];
+
+        ArrayList<ArrayList<DataObj>> quickSortLists = new ArrayList<>();
+        ArrayList<ArrayList<DataObj>> mergeSortLists = new ArrayList<>();
+        ArrayList<ArrayList<DataObj>> oddEvenLists = new ArrayList<>();
+        ArrayList<ArrayList<DataObj>> heapSortLists = new ArrayList<>();
+        ArrayList<ArrayList<DataObj>> bubbleSortLists = new ArrayList<>();
+
+        int[] bubbleSortSwaps = new int[3];
+        int[] oddEvenSwaps = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+
+            //quickSort timer
+            arrayToSort = chooseArray(arrayData, i);
+            startTimer = System.nanoTime();
+            quickSort(arrayToSort, 0, arrayToSort.size() - 1);
+            endTimer = System.nanoTime();
+            quickSortTimes[i] = (endTimer - startTimer) / 1_000_000_000.0;
+            ArrayList<DataObj> quickSortList = new ArrayList<>(arrayToSort);
+            quickSortLists.add(quickSortList);
+
+            //mergeSort timer
+            arrayToSort = chooseArray(arrayData, i);
+            startTimer = System.nanoTime();
+            //mergeSort(arrayToSort, 0, arrayToSort.size() - 1);
+            endTimer = System.nanoTime();
+            mergeSortTimes[i] = (endTimer - startTimer) / 1_000_000_000.0;
+            ArrayList<DataObj> mergeSortList = new ArrayList<>(arrayToSort);
+            mergeSortLists.add(mergeSortList);
+
+            //bubbleSort timer
+            arrayToSort = chooseArray(arrayData, i);
+            startTimer = System.nanoTime();
+            bubbleSortSwaps[i] = bubbleSort(arrayToSort, arrayToSort.size());
+            endTimer = System.nanoTime();
+            bubbleSortTimes[i] = (endTimer - startTimer) / 1_000_000_000.0;
+            ArrayList<DataObj> bubbleSortList = new ArrayList<>(arrayToSort);
+            bubbleSortLists.add(bubbleSortList);
 
 
+            //oddEven timer
+            arrayToSort = chooseArray(arrayData, i);
+            startTimer = System.nanoTime();
+            oddEvenSwaps[i] = transpositionSort(arrayToSort, arrayToSort.size());
+            endTimer = System.nanoTime();
+            oddEvenTimes[i] = (endTimer - startTimer) / 1_000_000_000.0;
+            ArrayList<DataObj> oddEvenList = new ArrayList<>(arrayToSort);
+            oddEvenLists.add(oddEvenList);
 
-}
+            //heapSort timer
+            arrayToSort = chooseArray(arrayData, i);
+            startTimer = System.nanoTime();
+            heapSort(arrayToSort, 0, arrayToSort.size() - 1);
+            endTimer = System.nanoTime();
+            heapSortTimes[i] = (endTimer - startTimer) / 1_000_000_000.0;
+            ArrayList<DataObj> heapSortList = new ArrayList<>(arrayToSort);
+            heapSortLists.add(heapSortList);
+        }
+
+        //Write to output file
+        File file = new File("analysis.txt");
+        boolean needsHeader = false;
+        if (!file.exists()) needsHeader = true;
+
+        FileOutputStream analysisFile = new FileOutputStream("analysis.txt", true);
+        FileOutputStream sortedFile = new FileOutputStream("sorted.txt", false); //will overwrite
+        PrintWriter sortedWriter = new PrintWriter(sortedFile);
+        PrintWriter analysisWriter = new PrintWriter(analysisFile);
+
+        //write results to analysis.txt file in csv format
+
+        //check if file is empty, if so add a header
+        if (needsHeader) analysisWriter.println(
+                "Lines Read," +
+                "Sorted ," +
+                "Random BST Insert Time," +
+                "Sorted AVL Insert Time," +
+                "Random AVL Insert Time");
+
+        analysisWriter.printf("%d,%.9f,%.9f,%.9f,%.9f\n",
+                numLines,
+                sortedBSTSearchTime,
+                randomBSTSearchTime,
+                sortedAVLSearchTime,
+                randomAVLSearchTime
+        );
+
+
+    }
 
